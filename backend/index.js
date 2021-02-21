387a58c549402e70f05abc3439c83d6dd6625c76
Jonathan Pulgarin <jonathanpulgarin@live.com>
2021-02-21 16:10:20 -0700

    Boilerplate app created

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const rp = require('request-promise');
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => (res.send('Hello World!')))
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
